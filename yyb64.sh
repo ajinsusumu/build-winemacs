47f9aed3074b9eda5f2066a46a2fe755e2da0059
ajinsusumu <ajinsusumu@gmail.com>
2013-08-01 19:04:31 +0800

    add xml

--- yyb64.sh
+++ yyb64.sh
@@ -6,7 +6,7 @@ logfilename=b64-$namebase.log
 cd /d/emacs.bzr
 #/c/cygwin/bin/sh -c "PATH=/bin:/usr/bin;bzr clean-tree --force --ignored --unknown --detritus;bzr revert;bzr pull" > /d/$logfilename 2>&1
 #patch -p1 < $DIR/x64.patch >> /d/$logfilename 2>&1
-#./autogen.sh >> /d/$logfilename 2>&1
+./autogen.sh >> /d/$logfilename 2>&1
 mv /d/bx64 /d/bx64.$namebase
 mkdir /d/bx64
 cd /d/bx64
