dd80b38a13ac47c4bac2fe46441f567b0611bb06
ajinsusumu <ajinsusumu@gmail.com>
2013-08-05 10:22:22 +0800

    run from cygwin

--- bemacs.sh
+++ bemacs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DIR="$( cd "$( /bin/dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$( /bin/dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 $DIR/sanitize.sh
-$DIR/b32.sh
-$DIR/b64.sh
-/c/cygwin/bin/bash $DIR/bcyg.sh
+$DIR/bcyg.sh
+/cygdrive/c/mingw/msys/1.0/bin/bash `/bin/cygpath -m $DIR/b32.sh`
+/cygdrive/c/mingw/msys/1.0/bin/bash `/bin/cygpath -m $DIR/b64.sh`
--- sanitize.sh
+++ sanitize.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+PATH=/bin:/usr/bin
 DIR="$( cd "$( /bin/dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PATH=/bin:/mingw/bin:$PATH
 namebase=$(date -u +\%Y\%m\%dt\%H\%M\%S).${RANDOM}
-logfilename=sanitize-$namebase.log
-cd /d/emacs.bzr
-/c/cygwin/bin/sh -c "PATH=/bin:/usr/bin;bzr clean-tree --force --ignored --unknown --detritus;bzr revert" > /d/$logfilename 2>&1
-/c/cygwin/bin/sh -c "PATH=/bin:/usr/bin;bzr pull" > /d/$logfilename 2>&1
-patch -p1 < $DIR/x64.patch >> /d/$logfilename 2>&1
-./autogen.sh >> /d/$logfilename 2>&1
+logfilename=/cygdrive/d/sanitize-$namebase.log
+cd /cygdrive/d/emacs.bzr
+(bzr clean-tree --force --ignored --unknown --detritus;bzr revert) > $logfilename 2>&1
+bzr pull >> $logfilename 2>&1
+patch -p1 < $DIR/x64.patch >> $logfilename 2>&1
+./autogen.sh >> $logfilename 2>&1
