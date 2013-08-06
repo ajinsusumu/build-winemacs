a85821d293822bab6e7034f62c79a7a7fb113f39
ajinsusumu <ajinsusumu@gmail.com>
2013-08-06 15:53:44 +0800

    ad build cygwin mingw64

--- bemacs.sh
+++ bemacs.sh
@@ -2,5 +2,6 @@
 DIR="$( cd "$( /bin/dirname "${BASH_SOURCE[0]}" )" && /bin/pwd )"
 $DIR/sanitize.sh
 $DIR/bcyg.sh
+$DIR/bcygm64.sh
 /cygdrive/c/mingw/msys/1.0/bin/bash `/bin/cygpath -m $DIR/b32.sh`
 /cygdrive/c/mingw/msys/1.0/bin/bash `/bin/cygpath -m $DIR/b64.sh`
