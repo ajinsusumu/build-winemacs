954dfb55dc60787526833f0953a9dc3b50920f76
ajinsusumu <ajinsusumu@gmail.com>
2013-08-15 12:02:12 +0800

    add gnutls

--- b64.sh
+++ b64.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-PATH=/bin:/c/strawberry/c/bin:/mingw/bin:$PATH
+PATH=/bin:/c/strawberry/c/bin:/mingw/bin:/usr/local/bin:$PATH
 namebase=$(date -u +\%Y\%m\%dt\%H\%M\%S).${RANDOM}
 logfilename=b64-$namebase.log
 mv ${BUILD}/bx64 ${BUILD}/bx64.$namebase
 mkdir ${BUILD}/bx64
 cd ${BUILD}/bx64
-CPPFLAGS='-D_WIN64 -DWIDE_EMACS_INT -DGLYPH_DEBUG=1' CFLAGS='-O0 -g3' ${SRC}/nt/msysconfig.sh --with-xml2 --host=x86_64-w64-mingw32 --enable-checking >> ${BUILD}/$logfilename 2>&1
+CPPFLAGS='-D_WIN64 -DWIDE_EMACS_INT -DGLYPH_DEBUG=1' CFLAGS='-O0 -g3' ${SRC}/nt/msysconfig.sh --with-xml2 --host=x86_64-w64-mingw32 --enable-checking --with-gnutls >> ${BUILD}/$logfilename 2>&1
 make >> ${BUILD}/$logfilename 2>&1
 make check >> ${BUILD}/$logfilename 2>&1
